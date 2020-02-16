ff4566ffe68136497369d156c7e2aa81f5590108
Leonard Ginters <leonard@linters.de>
2020-02-16 19:12:50 +0100

    Add more suggestions

--- oncode.ts
+++ oncode.ts
@@ -53,7 +53,7 @@ const languages: Languages = {
 				class: "||lesserOrGreaterBrackets" },
 			{ regex: { string: "(&lt;)", flags: "g", },
 				class: "||lesserOrGreaterBrackets" }],
-		suggestions: ["html", "body", "p", "span"]
+		suggestions: ["a", "abbr", "acronym", "address", "applet", "area", "article", "aside", "audio", "b", "base", "basefont", "bdi", "bdo", "big", "blockquote", "body", "br", "button", "canvas", "caption", "center", "cite", "code", "col", "colgroup", "data", "datalist", "dd", "del", "details", "dfn", "dialog", "dir", "div", "dl", "dt", "em", "embed", "fieldset", "figcaption", "figure", "font", "footer", "form", "frame", "frameset", "h1 to h6", "head", "header", "hr", "html", "i", "iframe", "img", "input", "ins", "kbd", "label", "legend", "li", "link", "main", "map", "mark", "meta", "meter", "nav", "noframes", "noscript", "object", "ol", "optgroup", "option", "output", "p", "param", "picture", "pre", "progress", "q", "rp", "rt", "ruby", "s", "samp", "script", "section", "select", "small", "source", "span", "strike", "strong", "style", "sub", "summary", "sup", "svg", "table", "tbody", "td", "template", "textarea", "tfoot", "th", "thead", "time", "title", "tr", "track", "tt", "u", "ul", "var", "video", "wbr", "accept", "accept-charset", "accesskey", "action", "align", "alt", "async", "autocomplete", "autofocus", "autoplay", "autosave", "bgcolor", "border", "buffered", "challenge", "charset", "checked", "cite", "class", "code", "codebase", "color", "cols", "colspan", "content", "contenteditable", "contextmenu", "controls", "coords", "crossorigin", "data", "data-*", "datetime", "default", "defer", "dir", "dirname", "disabled", "download", "draggable", "dropzone", "enctype", "for", "form", "formaction", "headers", "height", "hidden", "high", "href", "hreflang", "http-equiv", "icon", "id", "integrity", "ismap", "itemprop", "keytype", "kind", "label", "lang", "language", "list", "loop", "low", "manifest", "max", "maxlength", "media", "method", "min", "multiple", "muted", "name", "novalidate", "open", "optimum", "pattern", "ping", "placeholder", "poster", "preload", "radiogroup", "readonly", "rel", "required", "reversed", "rows", "rowspan", "sandbox", "scope", "scoped", "seamless", "selected", "shape", "size", "sizes", "slot", "span", "spellcheck", "src", "srcdoc", "srclang", "srcset", "start", "step", "style", "summary", "tabindex", "target", "title", "type", "value", "width", "wrap"]
 	},
 	css: {
 		colors: [{ regex: { string: "([{}])", flags: "g", },
@@ -149,7 +149,7 @@ class OnCode {
 			availableSuggestions.filter((suggestion: string): boolean => suggestion.includes(focusWord) && secondLevelSuggestions.indexOf(suggestion) === -1)
 			: [];
 		const declaredVariables: string[] = languages[this.language].variableDeclarers
-			.map((declarer: string): string[] => {
+			?.map((declarer: string): string[] => {
 				return this.editor.innerText
 					.match(new RegExp(`${declarer} ([a-zA-Z0-9]*)`, "g"))
 					?.map(declaredVariable => declaredVariable.replace(declarer, "").trim())
@@ -526,4 +526,4 @@ class OnCode {
 
 		this.currentTabOffset = offset.match(/\s{4}/g) ? offset.match(/\s{4}/g).length : 0;
 	}
-}
+}c
